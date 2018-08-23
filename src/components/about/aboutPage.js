4a9c8592d1ac554d3f4213754c7a78a59400a483
RajMohan R K <rajmohanrk95@gmail.com>
2018-08-23 19:20:21 +0530

    component view

---
+++ src/components/about/aboutPage.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var React = require("react");
+
+var About = React.createClass({
+    statics: {
+        willTransitionTo: function(transition, params, query, callback) {
+            if(!confirm('Are you sure you want to read a boring Page ?')){
+                transition.About();
+            } else {
+                callback();
+            }
+        },
+
+        willTransitionFrom: function(transition, component) {
+            if(!confirm('Are you sure you want to leave an intresting Page ?')){
+                transition.About();
+            }
+        }
+    },
+    render: function() {
+        return (
+            <div>
+                <h1>About</h1>
+                <p>
+                    This application uses the following technologies:
+                   <ul>
+                        <li>React</li>
+                        <li>React Router</li>
+                        <li>Flux</li>
+                        <li>Node</li>
+                        <li>Gulp</li>
+                        <li>Browserify</li>
+                        <li>Bootstrap</li>
+                    </ul>
+                </p>
+            </div>
+        );
+    } 
+});
+
+module.exports = About;
